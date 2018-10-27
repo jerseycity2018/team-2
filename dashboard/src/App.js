409a134a854599d2f7871612bb7afd7745a13b01
Xin Yuan <sheanyxy@gmail.com>
2018-10-27 01:48:25 -0400

    save before code breaks

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import VisitorNumber from './components/VisitorNumber'
 import Issues from './components/Issues'
 
+
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <VisitorNumber/>
-        <Issues/>
+      <div class='container'>
+        <div className="App" class="col">
+          <VisitorNumber/>
+        </div>
+
+        <div class="col">
+          <Issues/>
+        </div>
       </div>
     );
   }
--- dashboard/src/components/Issues.js
+++ dashboard/src/components/Issues.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
 import Select from 'react';
+
+const sectionStyle = {
+
+  'padding-top': '50px',
+  'padding-bottom': '50px',
+  "text-align": 'center'
+};
+
 class Issues extends Component {
+
 
 
   render() {
     return (
-      <div className="App">
-        <p>Catagories: </p>
-        <select>
-          <option value="garbege">Garbege</option>
-          <option value="noise">Noise</option>
-          <option value="misuse">Inappropriate Usage</option>
-          <option value="others">Others</option>
-        </select>
+      <section class="features" id="features" className="App" style={sectionStyle}>
+      <div class="container">
+
+        <div class="row section-heading text-center">
+          <h2>Categories</h2>
+          <select>
+            <option value="garbege">Garbege</option>
+            <option value="noise">Noise</option>
+            <option value="misuse">Inappropriate Usage</option>
+            <option value="others">Others</option>
+          </select>
+        </div>
+
+        <div class="row my-auto">
+
+          <div class="col-lg-4">
+            <div class="feature-item">
+              <p class="text-muted">This will be complain msg1</p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="feature-item">
+              <p class="text-muted">This will be complain msg2</p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="feature-item">
+              <p class="text-muted">This will be complain msg3</p>
+            </div>
+          </div>
+
+        </div>
       </div>
+    </section>
     );
   }
 }
