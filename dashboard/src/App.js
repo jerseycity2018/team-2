6660090d140c81cdc6ea41acdcc029f2da9b84fa
Xin Yuan <sheanyxy@gmail.com>
2018-10-26 21:36:44 -0400

    create Issues component

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import VisitorNumber from './components/VisitorNumber'
+import Issues from './components/Issues'
+
 class App extends Component {
 
   render() {
     return (
       <div className="App">
         <VisitorNumber/>
+        <Issues/>
       </div>
     );
   }
---
+++ dashboard/src/components/Issues.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Issues extends Component {
+
+  render() {
+    return (
+      <div className="App">
+
+        this is issues 
+      </div>
+    );
+  }
+}
+
+export default Issues;
