f15e0c8ac5d31deffe4553e0f8cce2cba7e32755
Xin Yuan <sheanyxy@gmail.com>
2018-10-27 04:54:56 -0400

    connect firebase

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -4,8 +4,19 @@ import TopHashtags from './components/TopHashtags'
 import Issues from './components/Issues'
 import Header from './components/Header'
 import LiveFeed from './components/LiveFeed'
+import firebase from 'firebase'
 class App extends Component {
-
+componentWillMount(){
+  var config = {
+    apiKey: "AIzaSyDJ_3plHwyFSqoKrL0VeqOAY8CeVaF27nE",
+    authDomain: "cpca-9d697.firebaseapp.com",
+    databaseURL: "https://cpca-9d697.firebaseio.com",
+    projectId: "cpca-9d697",
+    storageBucket: "cpca-9d697.appspot.com",
+    messagingSenderId: "1037412277078"
+  };
+  firebase.initializeApp(config);
+}
   render() {
     return (
       <div className="App">
--- dashboard/src/components/Issues.js
+++ dashboard/src/components/Issues.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
 import Select from 'react';
+import firebase from 'firebase'
 
-const data={};
-const msgBody={};
-let msgBox=
+
+let msgBox=(msg)=>
   <div style={{"flex":'1'}}>
     <div style={msgPadding}>
-      <p>This will be complain msgBody</p>
+      <p>{msg}</p>
     </div>
   </div>
 ;
 
 class Issues extends Component {
+
   constructor(props) {
     super(props);
-    this.state = {value: 'garbege' };
-
+    this.state = {value: 'Garbage', complaints:{Garbage:[]} };
     this.handleChange = this.handleChange.bind(this);
+
   }
+  componentDidMount() {
+    firebase.database().ref("Complaints").on("value", (snapshot)=>{
+      this.setState({complaints:snapshot.val()})
+    })
+  }
+
   handleChange(event) {
     this.setState({value: event.target.value});
   }
 
   render() {
+
     return (
       <section className="App" style={sectionStyle}>
       <div>
@@ -30,28 +38,22 @@ class Issues extends Component {
         <div>
           <h2>Categories</h2>
           <select value={this.state.value} onChange={this.handleChange}>
-            <option value="garbege">Garbege</option>
+            <option value="Garbage">Garbage</option>
             <option value="noise">Noise</option>
             <option value="misuse">Inappropriate Usage</option>
             <option value="others">Others</option>
           </select>
         </div>
-
         <div style={msgStyle}>
-          {msgBox}
-          {msgBox}
-          {msgBox}
-          {msgBox}
+          {Object.keys(this.state.complaints[this.state.value]).map(complaint=>{return (msgBox(this.state.complaints[this.state.value][complaint]))})}
         </div>
         <div style={msgStyle}>
-          {msgBox}
-          {msgBox}
-          {msgBox}
-          {msgBox}
+
         </div>
       </div>
     </section>
     );
+
   }
 }
 
