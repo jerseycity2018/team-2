1148992f57d5fd006f6b4a4bf1c2adda3ca786a2
Xin Yuan <sheanyxy@gmail.com>
2018-10-26 22:13:54 -0400

    create select

--- dashboard/src/components/Issues.js
+++ dashboard/src/components/Issues.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-
+import Select from 'react';
 class Issues extends Component {
+
 
   render() {
     return (
       <div className="App">
-
-        this is issues 
+        <p>Catagories: </p>
+        <select>
+          <option value="garbege">Garbege</option>
+          <option value="noise">Noise</option>
+          <option value="misuse">Inappropriate Usage</option>
+          <option value="others">Others</option>
+        </select>
       </div>
     );
   }
