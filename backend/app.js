de419adc67bda2f6cec203b28dd7d5356b7fbb15
syedmoiz20 <33766905+syedmoiz20@users.noreply.github.com>
2018-10-27 04:17:00 -0400

    Update app.js
    supporting more dates and hastags

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/tweets', (req, res) => {
+app.get('/tweets?start=201810260000', (req, res) => {
 
   var T = new Twit({
     consumer_key:        process.env.CONSUMER_KEY,
@@ -46,4 +46,126 @@ app.get('/tweets', (req, res) => {
 
 
 })
-app.listen(port, () => console.log(Example app listening on port ${port}!))
+app.get('/tweets?start=201810250000', (req, res) => {
+
+  var T = new Twit({
+    consumer_key:        process.env.CONSUMER_KEY,
+    consumer_secret:     process.env.CONSUMER_SECRET,
+    access_token:        process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            false,     // optional - requires SSL certificates to be valid.
+  })
+
+  var tweets = {
+    'all' : [],
+    hashtags: {}
+  }
+
+  var hashtagsDict = { }
+
+  T.get('tweets/search/30day/prodcpc', { fromDate: '201810250000', toDate: '201810260000', query: "place:central_park" },  function (err, data, response) {
+    tweets.all = data.results.filter(tweet => tweet.retweeted_status == undefined)
+    tweets.all.forEach(tweet => {
+      let hashtags = tweet.entities.hashtags
+      if (hashtags.length != 0){
+        hashtags.forEach( hashtag => {
+          if (hashtagsDict[hashtag.text] != undefined){
+              hashtagsDict[hashtag.text] = hashtagsDict[hashtag.text] +1
+          }else{
+              hashtagsDict[hashtag.text] = 1
+          }
+          
+        })
+      }
+    })
+    tweets.hashtags = hashtagsDict
+    res.send(tweets)
+
+  })
+
+
+
+})
+app.get('/tweets?start=201810240000', (req, res) => {
+
+  var T = new Twit({
+    consumer_key:        process.env.CONSUMER_KEY,
+    consumer_secret:     process.env.CONSUMER_SECRET,
+    access_token:        process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            false,     // optional - requires SSL certificates to be valid.
+  })
+
+  var tweets = {
+    'all' : [],
+    hashtags: {}
+  }
+
+  var hashtagsDict = { }
+
+  T.get('tweets/search/30day/prodcpc', { fromDate: '201810240000', toDate: '201810250000', query: "place:central_park" },  function (err, data, response) {
+    tweets.all = data.results.filter(tweet => tweet.retweeted_status == undefined)
+    tweets.all.forEach(tweet => {
+      let hashtags = tweet.entities.hashtags
+      if (hashtags.length != 0){
+        hashtags.forEach( hashtag => {
+          if (hashtagsDict[hashtag.text] != undefined){
+              hashtagsDict[hashtag.text] = hashtagsDict[hashtag.text] +1
+          }else{
+              hashtagsDict[hashtag.text] = 1
+          }
+          
+        })
+      }
+    })
+    tweets.hashtags = hashtagsDict
+    res.send(tweets)
+
+  })
+
+
+
+})
+app.get('/tweets?start=201810230000', (req, res) => {
+
+  var T = new Twit({
+    consumer_key:        process.env.CONSUMER_KEY,
+    consumer_secret:     process.env.CONSUMER_SECRET,
+    access_token:        process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            false,     // optional - requires SSL certificates to be valid.
+  })
+
+  var tweets = {
+    'all' : [],
+    hashtags: {}
+  }
+
+  var hashtagsDict = { }
+
+  T.get('tweets/search/30day/prodcpc', { fromDate: '201810230000', toDate: '201810240000', query: "place:central_park" },  function (err, data, response) {
+    tweets.all = data.results.filter(tweet => tweet.retweeted_status == undefined)
+    tweets.all.forEach(tweet => {
+      let hashtags = tweet.entities.hashtags
+      if (hashtags.length != 0){
+        hashtags.forEach( hashtag => {
+          if (hashtagsDict[hashtag.text] != undefined){
+              hashtagsDict[hashtag.text] = hashtagsDict[hashtag.text] +1
+          }else{
+              hashtagsDict[hashtag.text] = 1
+          }
+          
+        })
+      }
+    })
+    tweets.hashtags = hashtagsDict
+    res.send(tweets)
+
+  })
+
+
+
+})
